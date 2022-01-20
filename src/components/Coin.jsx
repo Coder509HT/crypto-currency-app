b11067ca5ef89407e8c9b2881b4d8773224158a9
codewithmarc <codewithmarc@gmail.com>
2022-01-20 10:29:18 -0500

    First Commit

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -3,16 +3,19 @@ import "../styles/Coin.css";
 const Coin = ({ coin }) => {
   return (
     <div className="coin">
+      <div className="coin-header">
+        <h4 className="coin-title">{coin.name}</h4>
+      </div>
       <img src={coin.icon} alt="coin-name" className="coin-img" />
       <div className="coin-aside">
-        <h4 className="coin-title desc">
-          Name: <span className="resp">{coin.name}</span>
-        </h4>
         <p className="desc">
           Symbol: <span className="resp">{coin.symbol}</span>
         </p>
         <p className="desc">
           Price: <span className="resp">{coin.price}</span>
+        </p>
+        <p className="desc">
+          Rank: <span className="resp">{coin.rank}</span>
         </p>
         {coin.websiteUrl !== undefined && (
           <p className="desc">
--- src/saga/coinSaga.js
+++ src/saga/coinSaga.js
@@ -1,10 +1,13 @@
 import { call, put, takeEvery } from "redux-saga/effects";
 import { setCoinsLoaded } from "../redux/coinReducer";
+import coinsList from "./coins.json";
 
 function* fetchingCoins() {
-  const response = yield call(() => fetch(process.env.REACT_APP_API_LINK));
-  const coins = yield response.json();
-  yield put(setCoinsLoaded(coins.coins));
+  // const response = yield call(() => fetch(process.env.REACT_APP_API_LINK));
+  // const coins = yield response.json();
+  // yield put(setCoinsLoaded(coins.coins));
+
+  yield put(setCoinsLoaded(coinsList));
 }
 
 function* coinSaga() {
